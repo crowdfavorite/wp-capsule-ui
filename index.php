abb6ae04c7f7008c925d5ec72a8d7bbef7e98020
mike waite <mikewaite@crowdfavorite.com>
2013-01-29 12:27:54 -0700

    updated Settings link to go to /wp/wp-admin

--- index.php
+++ index.php
@@ -54,20 +54,20 @@ wp_head();
 <nav class="mainNav">
 		<a href="index.php" class="logo"> </a>
 		<a href="index.php" class="home-link">Home</a>
-		<a href="index.php">New Post</a>
+		<a href="#" class="post-new-link"><span><?php _e('New Post', '.post-new-link'); ?></span></a>
 		<a href="index.php" class="projects">Projects
 			<ul class="project-list">
 				<li>@test</li>
 				<li>@social</li>
 				<li>@capsule</li>
-				<li>@threads</li>
+				<li>@tacos</li>
 				<li>@persnickety</li>
 				<li>@threads</li>
 				<li class="list-last">All Projects</li>
 			</ul>
 		</a>
 		<a href="index.php">Search</a>
-		<a href="index.php">Settings</a>
+		<a href="/wp/wp-admin/">Settings</a>
 </nav>
 
 <div id="wrap">
