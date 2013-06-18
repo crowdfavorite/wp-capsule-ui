66ed618ed1910666496af32de05af65896ddd2c5
Alex King <alex@alexking.org>
2013-06-17 18:04:47 -0600

    mode to prod for release

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@ function is_capsule_server() {
 
 function capsule_mode() {
 	if (!defined('CAPSULE_MODE')) {
-		define('CAPSULE_MODE', 'dev');
+		define('CAPSULE_MODE', 'prod');
 	}
 	return CAPSULE_MODE;
 }
